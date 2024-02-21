57db5f0dcbeff27583adf593686e554d376ffbe0
bharathneu <bhaskar.bh@northeastern.edu>
2024-02-21 15:07:17 -0500

    Firewall update  commit

--- main.tf
+++ main.tf
@@ -13,7 +13,7 @@ resource "google_compute_network" "vpc_name" {
 resource "google_compute_subnetwork" "webapp" {
   for_each = google_compute_network.vpc_name
   name          = "${each.key}-webapp"
-  ip_cidr_range = "10.1.0.0/24"
+  ip_cidr_range = var.webapp_subnet_cidr
   region        = "us-east1"
   network       = each.value.self_link
 }
@@ -22,7 +22,7 @@ resource "google_compute_subnetwork" "webapp" {
 resource "google_compute_subnetwork" "db" {
   for_each = google_compute_network.vpc_name
   name          = "${each.key}-db"
-  ip_cidr_range = "10.2.0.0/24"
+  ip_cidr_range = var.db_subnet_cidr
   region        = "us-east1"
   network       = each.value.self_link
 }
@@ -34,4 +34,53 @@ resource "google_compute_route" "webapp_route" {
     network               = each.value.self_link
     dest_range            = "0.0.0.0/0"
     next_hop_gateway      = "default-internet-gateway"
+}
+
+
+# Firewall rule to allow traffic to the application port and deny SSH
+resource "google_compute_firewall" "allow_application_traffic" {
+  for_each = google_compute_subnetwork.webapp
+  name    = "${each.key}-allow-application-traffic"
+  network = each.value.network
+
+  allow {
+    protocol = "tcp"
+    ports    = ["8080"] # Replace with your application's port
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+}
+
+resource "google_compute_firewall" "deny_ssh" {
+  for_each = google_compute_subnetwork.webapp
+  name    = "${each.key}-deny-ssh"
+  network = each.value.network
+
+  deny {
+    protocol = "tcp"
+    ports    = ["22"]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+}
+
+#Compute Engine instance with a custom boot disk
+resource "google_compute_instance" "custom_instance" {
+  for_each = google_compute_subnetwork.webapp
+  name         = "${each.key}-instance"
+  machine_type = "n1-standard-1" 
+  zone         = "us-east1-b"    
+
+  network_interface {
+    subnetwork = each.value.self_link
+  }
+
+  boot_disk {
+    initialize_params {
+      image = var.custom_image  # dynamic variable for image url
+      type  = "pd-balanced"
+      size  = 100
+    }
+  }
+  
 }
--- variables.tf
+++ variables.tf
@@ -2,7 +2,7 @@
 variable "project_id" {
   type    = string
   description = "The Google Cloud project ID."
-  default = "cloud6225vpc"
+  default = "dev6225webapp"
 }
 
 
@@ -11,3 +11,21 @@ variable "vpc_name" {
   type        = list(string)
   default     = ["cloudvpc-vpc"]
 }
+
+variable "webapp_subnet_cidr" {
+  description = "The IP CIDR range for the webapp subnet"
+  type        = string
+  default     = "10.1.0.0/24"
+}
+
+variable "db_subnet_cidr" {
+  description = "The IP CIDR range for the db subnet"
+  type        = string
+  default     = "10.2.0.0/24"
+}
+
+variable "custom_image" {
+  description = "The custom image for the boot disk of the compute instance"
+  type        = string
+}
+
